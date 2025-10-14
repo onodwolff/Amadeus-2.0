2ed6233e3fd4ff1d06263e848aa041757a504420
Volkov Zheka <87943906+onodwolff@users.noreply.github.com>
2025-10-14 20:23:26 +0200

    Fix MFA migration dependencies

--- backend/gateway/alembic/versions/43bed44187a2_merge_audit_and_user_token_heads.py
+++ backend/gateway/alembic/versions/43bed44187a2_merge_audit_and_user_token_heads.py
@@ -1,7 +1,7 @@
 """Merge audit and user token heads
 
 Revision ID: 43bed44187a2
-Revises: 3bd5b0e1f5a5, 4ac08f7fba12
+Revises: 8d8c8cfa3f42, 4ac08f7fba12
 Create Date: 2024-08-13 00:00:00.000000
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "43bed44187a2"
-down_revision = ("3bd5b0e1f5a5", "4ac08f7fba12")
+down_revision = ("8d8c8cfa3f42", "4ac08f7fba12")
 branch_labels = None
 depends_on = None
 
