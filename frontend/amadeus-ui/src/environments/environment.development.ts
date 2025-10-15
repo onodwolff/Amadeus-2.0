221808896beafb592e6df15084e82a82104dec32
Volkov Zheka <87943906+onodwolff@users.noreply.github.com>
2025-10-16 00:41:12 +0200

    Update development OAuth configuration

--- frontend/amadeus-ui/src/environments/environment.development.ts
+++ frontend/amadeus-ui/src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8000',
   oauth: {
-    issuer: 'http://localhost:8000/realms/amadeus',
-    clientId: 'amadeus-ui',
+    issuer: 'http://localhost:8080/realms/amadeus',
+    clientId: 'amadeus-spa',
     redirectUri: 'http://localhost:4200',
     scope: 'openid profile email offline_access',
   },
